fc236e98923c6a91b9c334ba7876679ea3b3adef
yunjoo <anuefn@naver.com>
2020-09-24 21:24:04 +0900

    models.py edited

--- sisasisa/admin.py
+++ sisasisa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import news_infos
+from .models import words
+from .models import assoicated_words
+from .models import amounted_mentions
+from .models import user_scrap
 
-# Register your models here.
+admin.site.register(news_infos)
+admin.site.register(words)
+admin.site.register(assoicated_words)
+admin.site.register(amounted_mentions)
+admin.site.register(user_scrap)
--- sisasisa/models.py
+++ sisasisa/models.py
@@ -11,10 +11,22 @@ class news_infos(models.Model):
     provider_link_page = models.CharField(max_length=300, null=True)
     published_at = models.DateTimeField(blank=True, null=True)
 
+    def __str__(self):
+        return self.news_id
+
+    def publish(self):
+        self.save()
+
 
 class words(models.Model):
     word = models.CharField(max_length=30)
     meaning = models.TextField()
+
+    def __str__(self):
+        return self.word
+
+    def publish(self):
+        self.save()
 
 
 class assoicated_words(models.Model):
@@ -22,13 +34,31 @@ class assoicated_words(models.Model):
     word = models.CharField(max_length=30)
     weight = models.DecimalField(max_digits=5,decimal_places=2)
 
+    def __str__(self):
+        return self.word
+
+    def publish(self):
+        self.save()
+
 
 class amounted_mentions(models.Model):
     wordId = models.CharField(max_length=30)
     label = models.CharField(max_length=10)
     hits = models.IntegerField()
 
+    def __str__(self):
+        return self.wordId
+
+    def publish(self):
+        self.save()
+
 
 class user_scrap(models.Model):
     user_Identifier = models.CharField(max_length=100)
     wordId = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.wordId
+
+    def publish(self):
+        self.save()
