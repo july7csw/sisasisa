a668923b8cacf35400735ef178f1cdd2943b46be
july7csw <fakereal@naver.com>
2020-10-07 20:35:20 +0900

    Add myscrap and scrap star icon

--- sisasisa/urls.py
+++ sisasisa/urls.py
@@ -5,13 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.hot_word, name='index'),
-    path('steady', views.steady, name='steady'),
-    path('hot', views.hot, name='hot'),
-    path('word_list', views.hot_word, name='hot_word'),
-    path('scrap', views.scrap, name='scrap'),
+    path('steady', views.steady_word, name='steady'),
+    path('hot', views.hot_word, name='hot'),
+    path('myscrap', views.myscrap, name='myscrap'),
+    path('search', views.search, name='search'),
+    # path('scrap', views.scrap, name='scrap'),
     # path('login', views.login, name='login'),
     # path('logout', views.logout, name='logout'),
-    path('hot', views.hot_word, name='hot'),
-    path('mypage', views.mypage, name='mypage'),
-    path('search', views.search, name='search')
 ]
--- sisasisa/views.py
+++ sisasisa/views.py
@@ -14,16 +14,14 @@ from .models import Words
 def index(request):
     return render(request, 'news_infos/index.html')
 
-
-def steady(request):
+def steady_word(request):
     words = rank.returnSteadyWord()
     return render(request, 'news_infos/steady.html', {'words': words})
 
-
-def hot(request):
-    words = rank.returnHotWord()
-    return render(request, 'news_infos/hot.html', {'list': words})
-
+def hot_word(request):
+    hotWords = rank.returnHotWord()
+    steadyWords = rank.returnSteadyWord()
+    return render(request, 'news_infos/index.html', {'steadyWords': steadyWords, 'hotWords': hotWords})
 
 def check_login(request):
     now_user = request.user
@@ -33,29 +31,15 @@ def check_login(request):
         logincheck = False
     return logincheck
 
-
-def news_infos(request):
-    return render(request, 'news_infos/index.html')
-
-
-def word_list(request):
-    words = Words.objects.all()
-    return render(request, 'words/word_list.html', {'word_list': words})
-
-
-def hot_word(request):
-    hotWords = rank.returnHotWord()
-    steadyWords = rank.returnSteadyWord()
-    return render(request, 'news_infos/index.html', {'steadyWords': steadyWords, 'hotWords': hotWords})
-
-
 def detail_word(request):
     word = Words.objects.get(word=request.__getattribute__('word'))
     meaning = word.__getattribute__('meaning')
     return render(request, 'words/detail_word.html', {'word': word, 'meaning': meaning})
 
+def login(request):
+    return render(request, 'member/../user/templates/user/login.html', {})
 
-def scrap(request):
+def myscrap(request):
     logincheck = check_login(request)
     if logincheck is True:
         list = User_scrap.objects.filter(user_Identifier=request.user)
@@ -64,31 +48,9 @@ def scrap(request):
         for i in wordIdList:
             word = Words.objects.get(id=i)
             words.append(word.word)
-        return render(request, 'words/scrap.html', {'scrapList': words})
-    else:
-        return render(request, 'member/../user/templates/user/login.html')
-
-
-def login(request):
-    return render(request, 'member/../user/templates/user/login.html', {})
-
-
-# def logout(request):
-#     return HttpResponseRedirect(request.POST['path'])
-    # django_logout(request)
-    # return redirect('index')
-
-
-def mypage(request):
-    this_user = request.user
-    message = "로그인이 필요한 페이지입니다."
-    if this_user.is_authenticated:
-        print(request.user.email)
-        user = "user 데이터를 가져온 뒤 처리"
-        return render(request, 'member/mypage.html', {'user': user})
+        return render(request, 'news_infos/myscrap.html', {'scrapList': words})
     else:
         return redirect('user/login')
-
 
 def search(request):
     keyword = request.GET.get('keyword', '')
@@ -117,3 +79,14 @@ def search(request):
             meaningResult = [ x for x in zip(inMeaning_word, inMeaning_mean)]
             return render(request, 'news_infos/search.html', {'inWord': inWord, 'meaningResult': meaningResult, 'keyword': keyword})
 
+
+
+# def mypage(request):
+#     this_user = request.user
+#     message = "로그인이 필요한 페이지입니다."
+#     if this_user.is_authenticated:
+#         print(request.user.email)
+#         user = "user 데이터를 가져온 뒤 처리"
+#         return render(request, 'member/mypage.html', {'user': user})
+#     else:
+#         return redirect('user/login')
