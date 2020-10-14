7e58f53b62d2622532b71ad02ddd0352294447aa
yunjoo <anuefn@naver.com>
2020-10-14 23:27:18 +0900

    fix keyword

--- sisasisa/templatetags/searchWord.py
+++ sisasisa/templatetags/searchWord.py
@@ -1,15 +1,13 @@
+import re
+
 from django import template
 from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.filter
 def addCss(value, keyword):
-    valueLower = value.lower()
-    keywordLower = value.lower()
-    keywordLength = len(keyword)
-    keywordIndex = valueLower.find(keywordLower)
-    keywordPart = value[keywordIndex:keywordIndex+keywordLength]
-    keywordText = "<strong>" + keywordPart + "</strong>"
-    finalText = value.replace(keywordPart, keywordText)
-    return mark_safe(finalText)
+    text = value.replace(keyword, '<strong>' + keyword + '</strong>')
+    text = mark_safe(text)
+    return text
