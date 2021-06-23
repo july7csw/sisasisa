dd3d4688ad0572fdd036b47898eb9423644e312f
Yxnzoo <anuefn@naver.com>
2021-06-23 23:39:28 +0900

    fix settings.py host

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '.ap-northeast-2.compute.amazonaws.com', '*.sisasisa.ga']
 
 # Application definition
 
